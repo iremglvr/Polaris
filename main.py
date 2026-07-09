273c9744c2dd79095c2f83c5bdcf05e47f014a7f
İrem Gülver <iremgulver@gmail.com>
2026-07-09 04:33:08 +0300

    Complete Bounce Day 4

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("Hello, I'm Po!");
-name = input("What's your name?");
-print(f"Nice to meet you, {name}!");
+# Project Polaris
+# Day 4
+# Founder: İrem
+
+print("Hello, I'm Po!")
+name = input("What's your name?")
+
+if name == "İrem":
+    print("Welcome back Founder!")
+else:
+    print(f"Nice to meet you, {name}!")
+
+age = input(f"How old are you, {name}?")
+print(f"Wow! You are {age} years old, {name}.")
