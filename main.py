6bf4e9c83be16cd3a2b38c11e7ca437b8f24a950
İrem Gülver <iremgulver@gmail.com>
2026-07-16 14:38:25 +0300

    Bounce day 6

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 print("Hello, I'm Po!")
 
+#get user information
 name = input("What's your name? ")
 
 if name == "İrem":
@@ -13,27 +14,34 @@ else:
 
 age = input(f"How old are you, {name}? ")
 
-print()
+menu=1
+#main menu loop
+while menu==1 :
+    print()
 
-print("====== PO ======")
-print("1 - Greetings")
-print("2 - Tell me my age")
-print("3 - Exit")
-print()
+    print("====== PO ======")
+    print("1 - Greetings")
+    print("2 - Tell me my age")
+    print("3 - Exit")
+    print()
 
-choice = input("Your choice: ")
+    choice = input("Your choice: ")
 
-if choice == "1":
-    if name == "İrem":
-        print("Welcome back Founder!")
+
+    #process user's selection
+    if choice == "1":
+        if name == "İrem":
+            print("Welcome back Founder!")
+        else:
+            print(f"Nice to meet you again, {name}!")
+
+    elif choice == "2":
+        print(f"You are {age} years old, {name}.")
+
+    elif choice == "3":
+        print(f"Goodbye, {name}!")
+        print("Have a great day!")
+        menu=0
+
     else:
-        print(f"Nice to meet you again, {name}!")
-
-elif choice == "2":
-    print(f"You are {age} years old, {name}.")
-
-elif choice == "3":
-    print(f"Goodbye, {name}!")
-
-else:
-    print("Invalid choice. Please try again.")
+        print("Invalid choice. Please try again.")
